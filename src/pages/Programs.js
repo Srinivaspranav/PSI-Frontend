6df8dc14550c1d3cdff223622557be83a4c02678
Pranav <srinivaspranav1031@gmail.com>
2024-03-02 18:39:07 +0530

    "program"

--- src/pages/Programs.js
+++ src/pages/Programs.js
@@ -10,36 +10,65 @@ import '../Style/scrollbar.css';
 
 
 
-const SingleImageSection = () => (
-  <div>
-    <img className="Image" src="https://cdn.discordapp.com/attachments/1205756212445192263/1205756751660453908/1.png?ex=65e2c1fb&is=65d04cfb&hm=fc8d5cfadee94fe6baaca53754fcff3bae73ac996c3758b515e6f92588c0514b&" alt="Single Image" />
-  </div>
-);
+// const SingleImageSection = () => (
+//   <div>
+//     <img className="Image" src="https://cdn.discordapp.com/attachments/1205756212445192263/1205756751660453908/1.png?ex=65e2c1fb&is=65d04cfb&hm=fc8d5cfadee94fe6baaca53754fcff3bae73ac996c3758b515e6f92588c0514b&" alt="Single Image" />
+//   </div>
+// );
 
 
 
 
-const ProgramSection = ({ title, imageUrl, content, isImageOnRight }) => {
-  const containerStyle = {
-    className: 'programSection',
-    style: {
-      flexDirection: isImageOnRight ? 'row-reverse' : 'row',
-    },
-  };
+// const ProgramSection = ({ title, imageUrl, content, isImageOnRight }) => {
+//   const containerStyle = {
+//     className: 'programSection',
+//     style: {
+//       flexDirection: isImageOnRight ? 'row-reverse' : 'row',
+//     },
+//   };
 
+//   return (
+//     <div style={containerStyle.style} className={containerStyle.className}>
+//       <img src={imageUrl} alt={title} className='image' />
+//       <div className='overlay'>
+//         <div className='text'>Overlay Text</div>
+//       </div>
+//       <div className='content'>
+//         <h2 className='programTitle'>{title}</h2>
+//         <p>{content}</p>
+//       </div>
+//     </div>
+//   );
+// };
+const Workshops = () => {
   return (
-    <div style={containerStyle.style} className={containerStyle.className}>
-      <img src={imageUrl} alt={title} className='image' />
-      <div className='overlay'>
-        <div className='text'>Overlay Text</div>
-      </div>
-      <div className='content'>
-        <h2 className='programTitle'>{title}</h2>
-        <p>{content}</p>
-      </div>
+    <div>
+      <article className="card_program">
+        <div className="temporary_text">
+          {/* Place image here */}
+        </div>
+        <div className="card_content">
+          <span className="card_title">This is a Title</span>
+          <span className="card_subtitle">This is a subtitle of this page. Let us see how it looks on the Web.</span>
+          <p className="card_description">Lorem ipsum dolor, sit amet expedita exercitationem recusandae aut dolor tempora aperiam itaque possimus at, cupiditate earum, quae repudiandae aspernatur? Labore minus soluta consequatur placeat.</p>
+        </div>
+      </article>
+
+      <article className="card_program">
+        <div className="temporary_text">
+          {/* Place image here */}
+        </div>
+        <div className="card_content">
+          <span className="card_title">This is a Title</span>
+          <span className="card_subtitle">This is a subtitle of this page. Let us see how it looks on the Web.</span>
+          <p className="card_description">Lorem ipsum dolor, sit amet expedita exercitationem recusandae aut dolor tempora aperiam itaque possimus at, cupiditate earum, quae repudiandae aspernatur? Labore minus soluta consequatur placeat.</p>
+        </div>
+      </article>
     </div>
   );
 };
+
+
 
 
 
@@ -49,27 +78,9 @@ const Missions = () => (
   <div className="app">
     <div>
       <NavBar />
-      <SingleImageSection />
-      <ProgramSection
-
-        imageUrl="https://cdn.discordapp.com/attachments/1205756212445192263/1205756751660453908/1.png?ex=65e2c1fb&is=65d04cfb&hm=fc8d5cfadee94fe6baaca53754fcff3bae73ac996c3758b515e6f92588c0514b&"
-
-      />
-      <ProgramSection
-
-        imageUrl="https://cdn.discordapp.com/attachments/1205756212445192263/1205756752927391756/4.png?ex=65e2c1fb&is=65d04cfb&hm=91d0af329371855d57f1068b84ee24174da1c0c9372079b6cf2af7dd058c3ae5&"
-
-      />
-      <ProgramSection
-
-        imageUrl="https://cdn.discordapp.com/attachments/1205756212445192263/1205756751660453908/1.png?ex=65e2c1fb&is=65d04cfb&hm=fc8d5cfadee94fe6baaca53754fcff3bae73ac996c3758b515e6f92588c0514b&"
-
-      />
-      <ProgramSection
-
-        imageUrl="https://cdn.discordapp.com/attachments/1205756212445192263/1205756752927391756/4.png?ex=65e2c1fb&is=65d04cfb&hm=91d0af329371855d57f1068b84ee24174da1c0c9372079b6cf2af7dd058c3ae5&"
-
-      />
+      
+      <Workshops />
+     
       <Footer />
     </div>
   </div>
