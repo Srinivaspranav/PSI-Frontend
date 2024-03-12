e1cc21607dfac96a2c7158b524c17b1fada686f3
Pranav <srinivaspranav1031@gmail.com>
2024-03-12 22:15:45 +0530

    "enroll responsive"

--- src/pages/enrollment.js
+++ src/pages/enrollment.js
@@ -20,16 +20,17 @@ const Enrollment = () => {
           <textarea
             style={{
               height: "200px",
-              width: "100%",
+              width: "91%",
               marginTop: "4%",
+              marginRight:'0%',
               border: "dashed",
               backgroundColor: "#002333", 
               color: "#ffffff" 
             }}
             placeholder='How can we Help you?'
           ></textarea>
-          <div style={{ marginTop: "1%", marginLeft: "-5%" ,width:'120%'}}>
-          <input style={{ marginLeft: "-22.5%" }} id='t&c' type='checkbox' required /><label for="t&c" style={{ marginLeft: "-22.5%", color: "#00FF84" }}>I have read Dr.Pallemsetty Innovations Privacy Policy and agree to the terms and conditions.</label>
+          <div style={{ marginTop: "1%", marginLeft: "0%" ,width:'100%'}}>
+          <input style={{ marginLeft: "-17%" }} id='t&c' type='checkbox' required /><label for="t&c" style={{ marginLeft: "-14.9%", color: "#00FF84" }}>I have read Dr.Pallemsetty Innovations Privacy Policy and agree to the terms and conditions.</label>
           </div>
           <button className='submit-btn'>SUBMIT</button>
         </div>
